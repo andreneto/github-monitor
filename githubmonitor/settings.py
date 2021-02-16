be0fd2fffceba1e84ee4233b211afa7fd8085c56
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-16 20:27:00 -0300

    Add pipeline for github profile creation

--- githubmonitor/settings.py
+++ githubmonitor/settings.py
@@ -150,6 +150,19 @@ SOCIAL_AUTH_GITHUB_KEY = config("SOCIAL_AUTH_GITHUB_KEY")
 SOCIAL_AUTH_GITHUB_SECRET = config("SOCIAL_AUTH_GITHUB_SECRET")
 SOCIAL_AUTH_GITHUB_SCOPE = ["public_repo", "write:repo_hook"]
 
+SOCIAL_AUTH_PIPELINE = (
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
+    "social_core.pipeline.social_auth.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
+    "repositories.pipeline.create_github_profile",
+)
+
 AUTHENTICATION_BACKENDS = (
     "social_core.backends.github.GithubOAuth2",
     "django.contrib.auth.backends.ModelBackend",
---
+++ repositories/pipeline.py
@@ -0,0 +1,16 @@
+from .models import GitHubProfile
+
+
+def create_github_profile(backend, response, user=None, *args, **kwargs):
+    if backend.name == "github":
+        if user:
+            GitHubProfile.objects.update_or_create(
+                github_id=response["id"],
+                user=user,
+                defaults={
+                    "name": response.get("name", response["username"]),
+                    "username": response["login"],
+                    "avatar": response.get("avatar_url", ""),
+                    "url": response["html_url"],
+                },
+            )
