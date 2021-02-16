11dc0a774a0ad5b48e88cd3329f4d4d80b5e7b86
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-16 01:54:18 -0300

    Refactor repositories models

--- repositories/admin.py
+++ repositories/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
 
-from .models import Commit, Repository
+from .models import GitHubProfile, Repository, Commit
+
+
+class GitHubProfileAdmin(admin.ModelAdmin):
+    list_filter = ("user",)
+    list_display = ("username", "name", "github_id")
+
+
+class RepositoryAdmin(admin.ModelAdmin):
+    list_filter = ("owner",)
+    list_display = ("owner", "name", "github_id")
 
 
 class CommitAdmin(admin.ModelAdmin):
     list_filter = ("repository",)
-    list_display = ("message", "date")
+    list_display = ("message", "authored_at")
 
 
+admin.site.register(GitHubProfile, GitHubProfileAdmin)
+admin.site.register(Repository, RepositoryAdmin)
 admin.site.register(Commit, CommitAdmin)
-admin.site.register(Repository)
--- repositories/models.py
+++ repositories/models.py
@@ -1,8 +1,31 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class GitHubProfile(models.Model):
+    github_id = models.BigIntegerField(unique=True)
+    name = models.CharField(max_length=100, blank=True)
+    username = models.CharField(max_length=100, unique=True)
+    avatar = models.URLField(max_length=200, blank=True)
+    url = models.URLField(max_length=200)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
 
 
 class Repository(models.Model):
+    owner = models.ForeignKey(
+        "repositories.GitHubProfile",
+        on_delete=models.CASCADE,
+        related_name="repositories",
+    )
     name = models.CharField(max_length=100)
+    github_id = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -14,10 +37,29 @@ class Repository(models.Model):
 class Commit(models.Model):
     message = models.TextField()
     sha = models.CharField(max_length=100)
-    author = models.CharField(max_length=50)
     url = models.URLField(max_length=200)
-    date = models.DateTimeField()
-    avatar = models.URLField(max_length=200, blank=True)
+
+    author_name = models.CharField(max_length=50)
+    author_email = models.EmailField()
+    authored_at = models.DateTimeField()
+    author_profile = models.ForeignKey(
+        "repositories.GitHubProfile",
+        on_delete=models.CASCADE,
+        related_name="authored_commits",
+        null=True,
+        blank=True,
+    )
+
+    committer_name = models.CharField(max_length=50)
+    committer_email = models.EmailField()
+    committed_at = models.DateTimeField()
+    committer_profile = models.ForeignKey(
+        "repositories.GitHubProfile",
+        on_delete=models.CASCADE,
+        related_name="committed_commits",
+        null=True,
+        blank=True,
+    )
 
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
 
@@ -25,4 +67,4 @@ class Commit(models.Model):
         return self.message
 
     class Meta:
-        ordering = ("-date",)
+        ordering = ("-authored_at",)
