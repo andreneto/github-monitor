f7ce916af54f4acdc7911453ecbeda49afc23f80
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-16 03:59:49 -0300

    Fix services

--- repositories/services.py
+++ repositories/services.py
@@ -6,6 +6,10 @@ class RemoteAPIError(Exception):
     pass
 
 
+class MalformedResponseError(RemoteAPIError):
+    pass
+
+
 class RepositoryNotFoundError(RemoteAPIError):
     pass
 
@@ -19,21 +23,46 @@ class RepositoryService:
     def add_repository(
         cls, username: str, repo_name: str, commits_since=None, commits_until=None
     ):
+        repo_data = cls._fetch_repository_data(username=username, repo_name=repo_name)
+
+        try:
+            owner_profile, created = models.GitHubProfile.objects.get_or_create(
+                github_id=repo_data["owner"]["id"],
+                defaults={
+                    "name": repo_data["owner"]["login"],
+                    "username": repo_data["owner"]["login"],
+                    "avatar": repo_data.get("owner", {}).get("avatar_url", ""),
+                    "url": repo_data["owner"]["html_url"],
+                },
+            )
+
+            repository = models.Repository.objects.create(
+                owner=owner_profile,
+                name=repo_data["name"],
+                github_id=repo_data["id"],
+                description=repo_data.get("description", ""),
+            )
+        except KeyError as err:
+            raise MalformedResponseError from err
+
         commits = cls._fetch_repository_commits(
             username=username,
             repo_name=repo_name,
             since=commits_since,
             until=commits_until,
         )
-        repository = models.Repository.objects.create(name=repo_name)
+
         commits = models.Commit.objects.bulk_create(
-            [cls._prepare_commit(commit, repository) for commit in commits]
+            [
+                models.Commit(**cls._prepare_commit(commit, repository))
+                for commit in commits
+            ]
         )
 
         return repository
 
-    @classmethod
-    def _fetch_repository_data(cls, username, repo_name):
+    @staticmethod
+    def _fetch_repository_data(username, repo_name):
         try:
             repo = github.get_repo(username=username, repository=repo_name)
         except github.NotFoundError as not_found:
@@ -43,9 +72,8 @@ class RepositoryService:
 
         return repo
 
-    @classmethod
+    @staticmethod
     def _fetch_repository_commits(
-        cls,
         username: str,
         repo_name: str,
         since=None,
@@ -75,22 +103,50 @@ class RepositoryService:
 
         return commits
 
-    @classmethod
-    def _prepare_commit(cls, commit, repository):
-        try:
-            avatar = commit["author"]["avatar"]
-        except KeyError:
-            try:
-                avatar = commit["commiter"]["avatar"]
-            except KeyError:
-                avatar = ""
+    @staticmethod
+    def _prepare_commit(commit, repository):
 
-            return {
-                "message": commit["commit"]["message"],
-                "sha": commit["sha"],
-                "author": commit["commit"]["author"]["email"],
-                "url": commit["commit"]["url"],
-                "date": commit["author"]["date"],
-                "avatar": avatar,
-                "repository": repository,
-            }
+        try:
+            author_profile, created = models.GitHubProfile.objects.get_or_create(
+                github_id=commit["author"]["id"],
+                defaults={
+                    "name": commit["author"]["login"],
+                    "username": commit["author"]["login"],
+                    "avatar": commit.get("author", {}).get("avatar_url", ""),
+                    "url": commit["author"]["html_url"],
+                },
+            )
+        except KeyError as err:
+            if err.args[0] != "author":
+                raise MalformedResponseError from err
+            author_profile = None
+
+        try:
+            committer_profile, created = models.GitHubProfile.objects.get_or_create(
+                github_id=commit["committer"]["id"],
+                defaults={
+                    "name": commit["committer"]["login"],
+                    "username": commit["committer"]["login"],
+                    "avatar": commit.get("committer", {}).get("avatar_url", ""),
+                    "url": commit["committer"]["html_url"],
+                },
+            )
+        except KeyError as err:
+            if err.args[0] != "committer":
+                raise MalformedResponseError from err
+            committer_profile = None
+
+        return {
+            "message": commit["commit"]["message"],
+            "sha": commit["sha"],
+            "url": commit["commit"]["url"],
+            "author_name": commit["commit"]["author"]["name"],
+            "author_email": commit["commit"]["author"]["email"],
+            "authored_at": commit["commit"]["author"]["date"],
+            "author_profile": author_profile,
+            "committer_name": commit["commit"]["committer"]["name"],
+            "committer_email": commit["commit"]["committer"]["email"],
+            "committed_at": commit["commit"]["committer"]["date"],
+            "committer_profile": committer_profile,
+            "repository": repository,
+        }
