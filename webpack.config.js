e3ef716725650a527bdead89b09aaf18434408a9
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-17 04:59:53 -0300

    Update js dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,27 +5,22 @@ var BundleTracker = require('webpack-bundle-tracker');
 module.exports = {
   mode: 'development',
   context: __dirname,
-  entry: './assets/js/index',
+  entry: ['babel-polyfill', './assets/js/index'],
   output: {
     path: path.resolve('./assets/bundles/'),
     filename: '[name]-[hash].js',
   },
 
-  plugins: [
-    new BundleTracker({ filename: './webpack-stats.json' }),
-  ],
+  plugins: [new BundleTracker({ filename: './webpack-stats.json' })],
 
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' },
     ],
   },
 
   resolve: {
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'assets/js'),
-    ],
-    extensions: ['.js', '.jsx']
+    modules: ['node_modules', path.resolve(__dirname, 'assets/js')],
+    extensions: ['.js', '.jsx'],
   },
 };
