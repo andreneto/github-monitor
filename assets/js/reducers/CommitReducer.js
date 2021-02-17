1fbf0c36187f8114315280e431ccbeafb40c2a3e
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-17 19:44:00 -0300

    Update commit reducer

--- assets/js/reducers/CommitReducer.js
+++ assets/js/reducers/CommitReducer.js
@@ -1,15 +1,33 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import commitAPI from '../services/api/commit';
+
+export const fetchCommitList = createAsyncThunk(
+  'commits/fetchCommitList',
+  async (page = 1, repositoryId = '', authorId = '') => {
+    const response = await commitAPI.listCommits(page, repositoryId, authorId);
+    return response.data;
+  }
+);
 
 const initialState = {
   commits: [],
-  successMessage: false,
+  commitCount: 0,
+  prevPage: null,
+  nextPage: null,
 };
 
 const commitsSlice = createSlice({
   name: 'commits',
   initialState,
-  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCommitList.fulfilled, (state, action) => ({
+      ...state,
+      commits: action.payload.results,
+      commitCount: action.payload.count,
+      prevPage: action.payload.previous,
+      nextPage: action.payload.next,
+    }));
+  },
 });
 
-export const { commitsRequested } = commitsSlice.actions;
 export default commitsSlice.reducer;
