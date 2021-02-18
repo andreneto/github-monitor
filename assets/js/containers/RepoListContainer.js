543b3575b20d1c7023012627c4e930dc2aa2f335
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-18 01:00:03 -0300

    Add repo list defaults

--- assets/js/containers/RepoListContainer.js
+++ assets/js/containers/RepoListContainer.js
@@ -2,35 +2,49 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import RepoList from '../components/RepoList';
-import { fetchRepositories } from '../reducers/RepositoryReducer';
+import { fetchRepositoryList } from '../reducers/RepositoryReducer';
 
 class RepoListContainer extends React.Component {
   componentDidMount() {
-    const { fetchRepositories } = this.props;
-    fetchRepositories();
+    const { fetchRepositoryList } = this.props;
+    fetchRepositoryList();
   }
 
   render() {
-    const { repositories } = this.props;
+    const { repositories, repositoryCount, prevPage, nextPage } = this.props;
     return (
       <div>
-        <RepoList repositories={repositories.results || []} />
+        <RepoList
+          repositories={repositories}
+          repositoryCount={repositoryCount}
+          prevPage={prevPage}
+          nextPage={nextPage}
+        />
       </div>
     );
   }
 }
 
 RepoListContainer.propTypes = {
+  repositoryCount: PropTypes.number.isRequired,
+  nextPage: PropTypes.string,
+  prevPage: PropTypes.string,
   repositories: PropTypes.arrayOf(PropTypes.object).isRequired,
-  fetchRepositories: PropTypes.func.isRequired,
+  fetchRepositoryList: PropTypes.func.isRequired,
+};
+
+RepoListContainer.defaultProps = {
+  nextPage: '',
+  prevPage: '',
 };
 
 const mapStateToProps = (store) => ({
   repositories: store.repositoryState.repositories,
+  repositoryCount: store.repositoryState.repositoryCount,
 });
 
 const mapDispatchToProps = {
-  fetchRepositories,
+  fetchRepositoryList,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RepoListContainer);
