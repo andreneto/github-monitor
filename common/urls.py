10b679a764038b473074457d674ecd78a4a1d793
Andre Carneiro <jacn@cin.ufpe.br>
2021-02-18 00:51:39 -0300

    Improve route stability

--- common/urls.py
+++ common/urls.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.views import LogoutView
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import HomeView, LoginView
 
 app_name = "common"
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home"),
     path("login", LoginView.as_view(), name="login"),
     path("logout", LogoutView.as_view(), name="logout"),
+    path("", HomeView.as_view(), name="home"),
+    re_path(r"^(?P<path>.*)/$", HomeView.as_view(), name="home_fallback"),
 ]
